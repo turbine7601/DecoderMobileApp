eb0f8f520c4b5be740b3fb453f50b0da37c1c00a
dankap09 <dankap09@mail.ru>
2025-04-28 11:49:01 +0700

    first commit

---
+++ Decoder.py
@@ -0,0 +1,69 @@
+
+class Decoder:
+    @staticmethod
+    def decode(text):
+        print(text)
+    @staticmethod    
+    def decode_caesar(text:str, shift:int, *args):
+        newtext = ''
+        if not str(shift.isdigit):
+            shift = -1
+        else:
+            shift = int(shift)-2*int(shift)
+
+        for i in text:
+            if i.isalpha():
+                d = shift % 26
+                if i.islower():
+                    if ord(i)+d > ord('z'):
+                        newtext += chr((ord(i)+d)-26)
+                    elif ord(i)+d < ord('A'):
+                        newtext += chr((ord(i)+d)+26)
+                    else:
+                        newtext += chr(ord(i)+d)
+                else:
+                    if ord(i)+d > ord('Z'):
+                        newtext += chr((ord(i)+d)-26)
+                    elif ord(i)+d < ord('A'):
+                        newtext += chr((ord(i)+d)+26)
+                    else:
+                        newtext += chr(ord(i)+d)
+            else:
+                newtext += i
+        return(newtext)
+    
+    @staticmethod
+    def decode_caesar_ru(text, shift, *args):
+        newtext, n = [], ""
+
+        if not str(shift.isdigit):
+            shift = -1
+        else:
+            shift = int(shift)-2*int(shift)
+
+        
+        dicti, dictiUpper = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя", "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+        for i in range(len(text)):
+            if text[i] in dicti:
+                n = dicti
+            elif text[i] in dictiUpper:
+                n = dictiUpper
+            else:
+                newtext.append(text[i])
+            if text[i] in n:
+                for j in range(len(n)):
+                    if 0 <= j + shift < len(n) and text[i] == n[j]:
+                        newtext.append(n[j+shift])
+                    elif j + shift >= len(n) and text[i] == n[j]:
+                        newtext.append(n[(1-j-shift)%(len(n)-1)])
+                    elif j + shift < 0 and text[i] == n[j]:
+                        newtext.append(n[(j+shift)%len(n)])
+        return "".join(newtext)
+        
+
+
+    
+
+
+if __name__ == "main":
+    pass
---
+++ DecoderMobileApp.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+from Decoder import Decoder
+
+Builder.load_file('DecoderInterface.kv')
+
+class MainScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class CaesarView(Screen):
+    translator = Decoder.decode_caesar
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @classmethod
+    def clicked(self):
+        txt = self.ids.text_to_decode.text #текст для расшифровки
+        dlt = self.ids.shift.text or "1" #сдвиг
+        self.ids.dtext.text = self.translator(text=txt, shift=dlt)
+
+class MainApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(name='menu'))
+        sm.add_widget(CaesarView(name='caesar'))
+        return sm
+    
+
+MainApp().run()
